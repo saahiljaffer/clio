9878201f6b3956828cbfe74cce137a78682e8aaf
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-01-07 23:03:54 -0500

    Added buttons etc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
 
 function Search({ setTitle }) {
   const onChange = (event) => {
@@ -18,20 +19,32 @@ function Search({ setTitle }) {
   );
 }
 
-function Results({ title }) {
+function Results({ title, page, updateNominated }) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const nominate = (event) => {
+    var data = event.target.value;
+    updateNominated((arr) => [...arr, `${arr.length}`]);
+  };
+
   useEffect(() => {
     if (!title) return;
-    setLoading(true);
-    fetch(`http://www.omdbapi.com/?s=${title}&apikey=b29472dd&type=movie`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [title]);
+    if (title.length >= 3) {
+      setLoading(true);
+      fetch(
+        `https://www.omdbapi.com/?s=${title}&apikey=b29472dd&type=movie&page=${page}`
+      )
+        .then((response) => response.json())
+        .then(setData)
+        .then(() => setLoading(false))
+        .catch(setError);
+    }
+  }, [page, title]);
+
+  if (title.length < 3)
+    return <p>Please enter a search query of at least 3 characters</p>;
 
   if (loading) return <p>Loading...</p>;
 
@@ -49,6 +62,30 @@ function Results({ title }) {
         {data.Search.map((data) => (
           <li key={data.imdbID}>
             {data.Title} - {data.Year}
+            <Button onClick={nominate} variant="dark" value={data.imdbID}>
+              Nominate
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Nominated({ nominated }) {
+  const remove = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <ul>
+        {nominated.map((movie) => (
+          <li key={movie}>
+            {movie.Title} - {movie.Year}
+            <Button onClick={remove} variant="dark" value={movie.imdbID}>
+              Nominate
+            </Button>
           </li>
         ))}
       </ul>
@@ -58,11 +95,14 @@ function Results({ title }) {
 
 function App() {
   const [title, setTitle] = useState("");
+  const [page, setPage] = useState(1);
+  const [nominated, updateNominated] = useState([]);
 
   return (
     <>
       <Search setTitle={setTitle} />
-      <Results title={title} />
+      <Results title={title} page={page} updateNominated={updateNominated} />
+      <Nominated nominated={nominated} />
     </>
   );
 }
