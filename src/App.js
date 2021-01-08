8589c0fa3a9764975ecad2a318c32c3916630a5e
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-01-07 20:59:59 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function Search({ setTitle }) {
+  const onChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        onChange={onChange}
+        type="text"
+        id="textInput"
+        placeholder="Movie Title"
+      />
+    </div>
+  );
+}
+
+function Results({ title }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!title) return;
+    setLoading(true);
+    fetch(`http://www.omdbapi.com/?s=${title}&apikey=b29472dd&type=movie`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [title]);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+  if (!data) return null;
+
+  console.log(data);
+
+  if (data.Response === "False") return <p>Movie Not Found</p>;
+
+  return (
+    <div>
+      <ul>
+        {data.Search.map((data) => (
+          <li key={data.imdbID}>
+            {data.Title} - {data.Year}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
+  const [title, setTitle] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Search setTitle={setTitle} />
+      <Results title={title} />
+    </>
   );
 }
 
